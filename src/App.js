b6820f48face55ab43c1db8c794abe938d264456
Greg Phillips <greg-phillips@live.co.uk>
2020-05-09 16:42:01 -0700

    prevented false log ins and empty registation. Moved API call to backend to protect API Key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -9,10 +8,6 @@ import Rank from './components/Rank/Rank';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import './App.css';
-
-const app = new Clarifai.App({
-	apiKey: '327bdc3d00d14faf85ec606fe5b75b15'
-});
 
 const particleOptions = {
 	particles: {
@@ -24,6 +19,21 @@ const particleOptions = {
 			}
 		}
 	}
+}
+
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signIn',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
 }
 
 class App extends Component {
@@ -78,9 +88,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
   	this.setState({imageUrl: this.state.input});
-  	app.models.predict(
-  		Clarifai.FACE_DETECT_MODEL,
-  		 this.state.input)
+  	fetch('http://localhost:3001/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
   	.then(response => {
       if (response) {
         fetch('http://localhost:3001/image', {
@@ -94,6 +109,7 @@ class App extends Component {
         .then(count => {
           this.setState(Object.assign(this.state.user, {entries: count}))
       })
+      .catch(console.log);
       }
       this.displayFaceBox(this.calculateFaceLocation(response))
 	  })
@@ -102,7 +118,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signOut') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
