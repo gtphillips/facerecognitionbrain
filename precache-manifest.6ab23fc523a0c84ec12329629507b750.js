0333727951bab4b4f21a3cf64108306e790b6828
Greg Phillips <greg-phillips@live.co.uk>
2020-05-11 17:41:45 -0700

    Updates

---
+++ precache-manifest.6ab23fc523a0c84ec12329629507b750.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9172949795eefd3e2b027e530d09bf91",
+    "url": "/smart-brain-gp/index.html"
+  },
+  {
+    "revision": "d914cb342df5f59a5c6d",
+    "url": "/smart-brain-gp/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "74c9e3b8448b8a865dbc",
+    "url": "/smart-brain-gp/static/css/main.63482c15.chunk.css"
+  },
+  {
+    "revision": "d914cb342df5f59a5c6d",
+    "url": "/smart-brain-gp/static/js/2.f76416f4.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/smart-brain-gp/static/js/2.f76416f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c9e3b8448b8a865dbc",
+    "url": "/smart-brain-gp/static/js/main.b1358be0.chunk.js"
+  },
+  {
+    "revision": "09895ee1cdd05cd34b3c",
+    "url": "/smart-brain-gp/static/js/runtime-main.8ff814b0.js"
+  },
+  {
+    "revision": "8c8d50ee13e1de2c8b16ad570f6cc5d8",
+    "url": "/smart-brain-gp/static/media/brain.8c8d50ee.png"
+  }
+]);
